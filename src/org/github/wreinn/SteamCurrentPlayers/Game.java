190d3bb3296013f4fd27275bbd08c66db0bc6fd0
Jordan <9260966+Wreinn@users.noreply.github.com>
2020-08-18 22:11:14 -0700

    Main functionality complete. Added some other sort orders, included stackoverflow links that I found useful.

---
+++ src/org/github/wreinn/SteamCurrentPlayers/Game.java
@@ -0,0 +1,107 @@
+package org.github.wreinn.SteamCurrentPlayers;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.Comparator;
+
+public class Game {
+    private int appID;
+    private String name;
+    private int playerCount;
+    private boolean failedPlayerCountLookup = false;
+
+    public static final Comparator<Game> NAME_ORDER_ASC;
+    public static final Comparator<Game> NAME_ORDER_DESC;
+    public static final Comparator<Game> PLAYER_ORDER_ASC;
+    public static final Comparator<Game> PLAYER_ORDER_DESC;
+
+    static {
+        NAME_ORDER_ASC = new Comparator<Game>() {
+            @Override
+            public int compare(Game o1, Game o2) {
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+        };
+        NAME_ORDER_DESC = new Comparator<Game>() {
+            @Override
+            public int compare(Game o1, Game o2) {
+                return o2.getName().compareToIgnoreCase(o1.getName());
+            }
+        };
+        PLAYER_ORDER_ASC = new Comparator<Game>() {
+            @Override
+            public int compare(Game o1, Game o2) {
+                if (o1.getPlayerCount() < o2.getPlayerCount()) {
+                    return -1;
+                } else if (o1.getPlayerCount() > o2.getPlayerCount()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+        PLAYER_ORDER_DESC = new Comparator<Game>() {
+            @Override
+            public int compare(Game o1, Game o2) {
+                if (o1.getPlayerCount() < o2.getPlayerCount()) {
+                    return 1;
+                } else if (o1.getPlayerCount() > o2.getPlayerCount()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+    }
+
+    public Game(int appID, String name) {
+        this.name = name;
+        this.appID = appID;
+    }
+
+    public int getAppID() {
+        return appID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPlayerCount() {
+        int playerCount = 0;
+
+        if (this.playerCount == 0 && ! failedPlayerCountLookup) {
+            try {
+                playerCount = playerCountFromSteamAPI(appID);
+            } catch (Exception e) {
+                failedPlayerCountLookup = true;
+                System.err.println(e);
+            }
+            this.playerCount = playerCount;
+        }
+
+        return this.playerCount;
+    }
+
+    private int playerCountFromSteamAPI(int gameID) throws IOException {
+        URL url = new URL("https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?format=json&appid="+gameID);
+        InputStream inputStream = url.openStream();
+        int playerCount = 0;
+
+        try {
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(inputStream, writer, "UTF-8");
+            JSONObject jsonObject = new JSONObject(writer.toString());
+            playerCount = jsonObject.getJSONObject("response").getInt("player_count");
+        } finally {
+            inputStream.close();
+        }
+
+        return playerCount;
+    }
+}
---
+++ src/org/github/wreinn/SteamCurrentPlayers/Main.java
@@ -0,0 +1,80 @@
+package org.github.wreinn.SteamCurrentPlayers;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args)  {
+        ArrayList<Game> games = gamesFromConfig();
+        populatePlayerCount(games, true);
+
+        ArrayList<Game> sortedGames = new ArrayList<>(games);
+        sortGames(sortedGames, Game.NAME_ORDER_ASC);
+
+        System.out.println("   Name PlayerCount");
+        for (Game game : sortedGames) {
+            System.out.printf("%7d %s\n", game.getPlayerCount(), game.getName());
+        }
+    }
+
+    private static ArrayList<Game> gamesFromConfig() {
+        Properties prop = new Properties();
+        String configFile = "config/app.properties";
+        InputStream isOne = null;
+        try {
+            isOne = new FileInputStream(configFile);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e);
+        }
+        try {
+            prop.load(isOne);
+        } catch (IOException e) {
+            System.err.println("IOException: " + e);
+        }
+
+        ArrayList<Game> gameList = new ArrayList<Game>();
+
+        for (Enumeration e = prop.keys(); e.hasMoreElements();) {
+            String gamekey = e.nextElement().toString();
+
+            Pattern pattern = Pattern.compile("game\\.(\\d+)$");
+            Matcher matcher = pattern.matcher(gamekey);
+
+            if (matcher.find() && matcher.groupCount() == 1) {
+                int appID = Integer.parseInt(matcher.group(1));
+                String gameName = prop.getProperty(gamekey);
+                gameList.add(new Game(appID, gameName));
+            }
+
+        }
+        return gameList;
+    }
+
+    private static void populatePlayerCount(ArrayList<Game> games, boolean showLoading) {
+        for (Game game : games) {
+            game.getPlayerCount();
+            if (showLoading) System.out.print('.');
+        }
+        if (showLoading) System.out.print('\n');
+    }
+
+    private static void sortGames(ArrayList<Game> games, Comparator<Game> comparator) {
+        games.sort(comparator);
+//        Collections.sort(games, comparator);
+    }
+}
+
+/*
+Some links that helped me:
+* https://stackoverflow.com/a/1359722
+* https://stackoverflow.com/a/35446009
+* https://stackoverflow.com/q/18410035
+* https://stackoverflow.com/a/15960913
+* https://stackoverflow.com/a/52016533
+* https://stackoverflow.com/a/48673336
+* https://stackoverflow.com/q/1647260
+* https://stackoverflow.com/q/1104975
+* https://stackoverflow.com/q/2279030
+ */
